1f83154f4998656df1f2c26cd5ab691f9bbb908b
Nathan Matthews <nathan.r.matthews@gmail.com>
2012-03-14 07:33:38 +0000

    Updated capture to re-program all clojure environment

--- src/capture.clj
+++ src/capture.clj
@@ -1,131 +1,110 @@
-(ns capture
-  (:use form-analysis
-        clojure.core.incubator
-        [clojure.walk :only (postwalk-replace)]
-        [clojure.repl :only (source-fn)]
-        ))
+(ns capture2
+  (:use [clojure.walk :only (macroexpand-all)])
+  (:import java.io.InputStreamReader
+           java.io.PushbackReader
+           java.io.LineNumberReader))
 
-; need a more sophisticated returns-fn?
+; fix-list
+(alter-var-root #'clojure.core/list (fn [list-fn] (fn [& args] (apply list-fn args))))
+(alter-meta! #'clojure.core/list dissoc :file :line)
 
-(defn- iterate-state 
-  "f is a function which takes state and returns [result new-state]"  
-  [f state]
-  (->> state f (iterate (comp f second)) (take-while identity) (map first))) 
- 
-(defn graph-seq [neighbours node]
-  (let [i (fn i [[f & r :as s] c v]
-            (cond
-              (empty? s) (if c (c v))
-              (v f) (i r c v)
-              :else [f [(neighbours f) (partial i r c) (conj v f)]]))]
-    (cons node (iterate-state (partial apply i)
-                              [(neighbours node) nil #{node}]))))
+(defn file-forms [filepath line-numbers]
+  (when-let [strm (.getResourceAsStream (clojure.lang.RT/baseLoader) filepath)]
+    (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
+      (doall
+        (map (fn [ln]
+               (dotimes [_ (- (dec ln) (.getLineNumber rdr))] (.readLine rdr))
+               (read (PushbackReader. rdr))) line-numbers)))))
 
-(defmacro fn* 
-  "Capture the form and attach as meta-data
-   and attach the local bindings"
-  [& forms]
-    (let [closures (vec (filter (free-vars (cons 'fn* forms)) (keys &env)))]
-    `(with-meta (fn* ~@forms) 
-       {:form '~&form
-        :closures (zipmap '~closures ~closures)
-        ;:tb (get-thread-bindings)
-        })))
+(defn attach-sym [sym]
+  (alter-var-root
+    (resolve sym)
+    #(vary-meta % assoc :symbol sym)))
 
-(def sym-ns-str #(.getNamespace %))
+(defn macro-var? [v] (:macro (meta v)))
 
-(def sym-ns #(-?> % sym-ns-str symbol find-ns)) 
+(defn fn-var? [v] (and (not (macro-var? v)) (fn? (var-get v))))
 
-(defn resolve-get [sym]
-  (if-let [v (resolve sym)]
-    (if (bound? v) (var-get v))))
+; question does the macro introduce a new fn?
+; just redef the forms with more than 1 fn, that will work
 
-(defn form [f] (-> f meta :form))
+(defn all-vars [] (mapcat (comp vals ns-interns) (all-ns)))
 
-(defn sym-var 
-  "Returns the var from a fully qualified symbol, 
-   doesn't attempt any resolution"
-  [sym]
-  (if-let [n (sym-ns sym)]
-    ((ns-interns n) (symbol (name sym)))))
+(defn forms [vars]
+	(reduce into {}
+		(map 
+		  (fn [[f vars]] 
+		    (let [line (comp :line meta)
+		          sorted-vars (sort-by line vars)]
+		      (zipmap sorted-vars (file-forms f (map line sorted-vars)))))    
+		  (group-by (comp :file meta) (filter (comp (every-pred :file :line) meta) vars)))))
 
-(defn var-obj [v] (if (bound? v) (var-get v)))
+(defn redefn []
+  (doall
+  (for [n (all-ns)]
+   (do
+    (println "Namespace:" n)
+    (binding [*ns* n]
+      (doall
+      (for [[v f] (forms (filter fn-var? (vals (ns-interns n))))]
+        (do
+          (println v)
+;        (if-not (->> f macroexpand-all vector flatten (filter (partial = 'fn*)) (drop 1) empty?)
+          (eval f)))))))))
 
-(defn ^:dynamic file-form [sym]
-  (if-let [s (source-fn sym)]
-    (expand-form (sym-ns sym) #{} (read-string s))))
+; remember to restore the meta-data
+; define a function which creates and caches itself?
+(defn lazy-fn [v form]
+  (fn [& args]
+    (println "In function")
+    (let [f (eval form)]
+      (alter-var-root v (fn [_] f))
+      (apply f args))))
 
-(defn form-syms 
-  "returns a seq of all the symbols within the form"
-  [form] (filter symbol? (flatten [form])))
+; attach symbols to start with?
+; fix list so that it can take meta data?
 
-;(defn returns-fn? [form]
-;  (->> form form-syms (filter (partial = 'fn*)) rest empty? not)) 
+(defn reval [n v full-form]
+  (alter-var-root v
+    (fn [old-fn]    
+      (binding [*ns* n]
+        (-> full-form
+	        macroexpand
+	        last
+	        eval
+	        (with-meta (meta old-fn)))))))
+                
+(defn lazy-reval [n v full-form]
+  (alter-var-root v
+    (fn [old-fn]
+      (with-meta
+        (fn [& args]
+          (apply (reval n v full-form) (apply args)))
+        (meta old-fn)
+        ))))
 
-(defn map-sym? [sym]
-  (-?> sym sym-var var-obj (every-pred fn? (complement form) file-form)))
+(defn attach-forms [vars]
+  (doall
+	  (map (fn [[v f]]
+	         (alter-var-root v (fn [o] (try
+                                      (vary-meta o assoc :form f)
+                                      (catch UnsupportedOperationException _ o)
+                                      (catch ClassCastException _ o)))))
+	    (forms vars))))
 
-(defn map-sym [sym]
-  (symbol (str "capture." (.getNamespace sym)) (name sym)))
+(defn test1 [] 1)
 
-(defn map-form [form]
-  (let [to-map (filter map-sym? (form-syms form))
-        old->new (zipmap to-map (map map-sym to-map))]
-    (postwalk-replace (assoc old->new 'fn* 'capture/fn*) form)))
+(defn test2 [] (test1))
 
-(defn intern! [sym]
-  (let [split-sym (juxt sym-ns-str name)
-	      [nns nname] (->> sym map-sym split-sym (map symbol))]
-    (intern (create-ns nns) nname sym)))
-
-(declare capture-fn)
-
-(defn capture-form [form]
-  (let [to-map (filter map-sym? (form-syms form))
-        old->new (zipmap to-map (map map-sym to-map))]
-    (dorun
-      (map (comp capture-fn first) (remove (comp resolve last) old->new)))
-    (postwalk-replace (assoc old->new 'fn* 'capture/fn*) form)))
-
-(defn capture-fn [sym]
-  (let [form (last (file-form sym))
-        mapped (map-sym sym)]
-    (alter-var-root 
-      (intern (sym-ns mapped) (name mapped))
-      (fn [_]
-        (binding [*ns* (sym-ns sym)]
-          (eval (capture-form form)))))))
-
-(def mapped? (comp resolve map-sym))
-
-(defn- to-redef [form]
-  (->> form form-syms distinct (filter map-sym?) (remove mapped?)))
-
-(defn all-to-redef [form]
-  (reduce into #{}
-    (map (partial graph-seq (comp to-redef file-form)) (to-redef form))))
-
-(defn reval! [sym]
-;  (println "sym:" sym)
-  (binding [*ns* (sym-ns sym)]
-    (-?> sym file-form last map-form eval 
-      (vary-meta assoc :source-fn sym))))
-
-(defn- 
-  capture*
-  "takes a form and converts it into something can can be remoted"
-  [env form]
-  (binding [file-form (memoize file-form)]
-	  (let [expanded (expand-form *ns* (or env #{}) form)
-	        domap (comp doall map)]
-	    (do
-	      (domap
-	        #(alter-var-root % reval!)
-	        (domap intern! (all-to-redef expanded)))
-	      (map-form expanded)))))
+;(defn attach-syms [syms]
+;  (doall
+;    (map (alter-var-root (resolve %) (var-meta  
   
-(defmacro
-  capture
-  "Add this macro around any form which requires functions being captured"
-  ([form] (capture* (keys &env) form))
-  ([form & more] (cons 'do (map (partial capture* (keys &env)) (cons form more)))))
+;(defn foobar []
+;  
+;  )
+
+(defmacro attach-def [form]
+  `(with-meta ~form {:form '~form}))
+
+; inside the capture macro bind all functions to their definition
